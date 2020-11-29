11973eb6b635b528cbc1944055a7a9b4ef845592
Rishi P <rishipatel1997@gmail.com>
2020-11-29 12:04:12 -0500

    Updated client and server interactions.

--- Server.py
+++ Server.py
@@ -1,5 +1,5 @@
 from flask import Flask
-from flask_socketio import SocketIO
+from flask_socketio import SocketIO, join_room, leave_room, send, emit
 
 
 app = Flask(__name__)
@@ -9,11 +9,62 @@ socketio = SocketIO(app)
 if __name__ == '__main__':
     socketio.run(app)
 
+# @socketio.on('connect')
+# def test_connect():
+#     emit('my response', {'data': 'Connected'})
+#
+# @socketio.on('disconnect')
+# def test_disconnect():
+#     print('Client disconnected')
 
-@socketio.on('my event')
-def handle_message(message):
-    #print("Recieved Message " + str(message))
-    print("test")
+
+rooms = {}
+
+@socketio.on('start game')
+def start_game(username, room):
+    print(username, room)
+
+
+@socketio.on('join room')
+def join_game(username, room):
+    print(username, room)
+
+    if not is_valid_username(username, room):
+        print("Invalid Username")
+    elif not is_valid_room(room):
+        print("Invalid Room")
+    elif room not in rooms.keys():
+        rooms[room] = [username]
+        join_room(room)
+    else:
+        if len(rooms[room]) == 1:
+            rooms[room].append(username)
+            join_room(room)
+    print(rooms)
+
+
+@socketio.on('leave room')
+def leave_game(username, room):
+    print(username, room)
+    if room not in rooms.keys():
+        print("Room is not in use")
+    elif username in rooms[room]:
+        rooms[room].remove(username)
+        leave_room(room)
+        if len(rooms[room]) == 0:
+            del rooms[room]
+    print(rooms)
+
+@socketio.on('draw game')
+def draw_game(username, room):
+    print(username, room)
+    # example of how to emit a message
+    emit("draw", username, room=room)
+
+@socketio.on('forfeit game')
+def forfeit_game(username, room):
+    print(username, room)
+
 
 # serves the index page
 @app.route('/')
@@ -31,3 +82,18 @@ def serve_file(filename):
     file_text = file.read()
     file.close()
     return file_text
+
+def is_valid_username(username, room):
+    if username == "":
+        return False
+    elif room not in rooms.keys():
+        return True
+    elif username in rooms[room]:
+        return False
+    else:
+        return True
+
+def is_valid_room(room):
+    if room == "":
+        return False
+    return True
